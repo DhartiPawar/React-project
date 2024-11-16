ab4faea653cf6310a1a4b5d7bca5a79bcae68fbc
dharti <dharatipawar0@gmail.com>
2024-11-16 23:23:15 +0530

    video 8

--- src/components/Textform.js
+++ src/components/Textform.js
@@ -7,19 +7,46 @@ export default function Textform(props) {
         let newText=text.toUpperCase();
         setText(newText)
     }
+    const handleLoClick = () => {
+      console.log("LowerCase was clicked");
+      let newText = text.toLowerCase();
+      setText(newText);
+    };
     const handleOnChange = (event) => {
       console.log("On Change");
       setText(event.target.value)
     };
-  const[text,setText]=useState("Enter text here");
+  const[text,setText]=useState("");
   return (
-    <div>
-    <h1>{props.heading}</h1>
-    <div className="mb-3">
-    {/* <label for="myBox" className="form-label">Example textarea</label> */}
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
-    </div>
-    <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
-    </div>
-  )
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          {/* <label for="myBox" className="form-label">Example textarea</label> */}
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>
+          Convert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>
+          Convert to LowerCase
+        </button>
+      </div>
+      <div className="container my-3">
+        <h2>Your Text summary</h2>
+        <p>
+          {text.split(" ").length} words {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length}Minutes read</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
+    </>
+  );
 }
